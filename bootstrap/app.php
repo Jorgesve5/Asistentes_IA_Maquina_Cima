f709543cb586cc8e5b5011d06418f29e58db70ee
jmelote2909 <jmelote2909@iesmarquesdecomares.org>
2026-05-26 07:51:06 +0200

    Chore: add global exception logging to bootstrap/app.php for easier debugging

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -14,5 +14,7 @@ return Application::configure(basePath: dirname(__DIR__))
         //
     })
     ->withExceptions(function (Exceptions $exceptions): void {
-        //
+        $exceptions->reportable(function (\Throwable $e) {
+            \Illuminate\Support\Facades\Log::error("Caught Global Exception: " . $e->getMessage() . "\n" . $e->getTraceAsString());
+        });
     })->create();
